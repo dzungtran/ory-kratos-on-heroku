2dea5c18cb57f1d4ef4ab6adf941eb7a626ba861
Dzung Tran <hello@iamdzung.com>
2022-05-21 16:54:43 +0700

    update file chmod and download script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 
 ## Download Kratos binary
 ## Current version is v0.9.0-alpha.3
-bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -d -b . kratos v0.9.0-alpha.3
+./download.sh kratos v0.9.0-alpha.3
 
 ## Run migrations
 ./kratos migrate sql $DATABASE_URL -y
