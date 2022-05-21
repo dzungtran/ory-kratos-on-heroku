2ca403311bfcb788a4adc16fbdcb8f92e038c7dc
Dzung Tran <hello@iamdzung.com>
2022-05-21 17:30:41 +0700

    fix build node heroku

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ## Download Kratos binary
 ## Current version is v0.9.0-alpha.3
-./download.sh kratos v0.9.0-alpha.3
+bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -d -b . kratos v0.9.0-alpha.3
 
 ## Run migrations
 ./kratos migrate sql $DATABASE_URL -y
