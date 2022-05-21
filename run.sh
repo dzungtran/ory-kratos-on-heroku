29ccc26355d9e40df2d9ce0b266f2f0e4bf21daf
Dzung Tran <hello@iamdzung.com>
2022-05-21 17:52:39 +0700

    update script to overwrite kratos.yml

--- run.sh
+++ run.sh
@@ -4,6 +4,16 @@
 ## Current version is v0.9.0-alpha.3
 bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -d -b . kratos v0.9.0-alpha.3
 
+## Overwrite kratos.yml
+if [ -z "$BASE64_ENCODED_KRATOS_CONFIG" ]
+then
+      echo "\$BASE64_ENCODED_KRATOS_CONFIG is empty"
+else
+      echo $BASE64_ENCODED_KRATOS_CONFIG | base64 -d > ./kratos.yml
+      echo "Overwrite kratos.yml"
+fi
+
+
 ## Run migrations
 ./kratos migrate sql $DATABASE_URL -y
 
